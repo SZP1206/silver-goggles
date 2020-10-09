f15364b6d0117a0952315ef653e41bbacfe52446
Jepson <szp106920@gmail.com>
2020-10-09 13:43:13 +0800

    完成了登陆功能

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+import Axios from 'axios'
 
+Vue.prototype.$http = Axios
+// 配置请求的根路径
+Axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
 Vue.config.productionTip = false
 
 new Vue({
